632ee1c29ef5eeec62db22fac0adff46f142c41d
Florent Xicluna <florent.xicluna@gmail.com>
2013-05-17 01:33:34 +0200

    Add second hello world

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,18 +1,9 @@
-from fiole import get, get_template, run_fiole
-
-index_template = get_template(source="""%require(name)
-<html>
-Hello World!<br><br>
-  Hello {{name|e}}<br><br>
-%for i in range(len(name)):
-  Hello {{name[:i+1]|e}}<br>
-%endfor
-</html>""")
+from fiole import get, run_fiole
 
 
 @get('/')
-@get('/(?P<name>\w*)')
-def index(request, name='stranger'):
-    return index_template.render(name=name)
+def index(request):
+    return 'Hello World!'
+
 
 run_fiole()
---
+++ examples/hello_world_tmpl.py
@@ -0,0 +1,19 @@
+from fiole import get, get_template, run_fiole
+
+index_template = get_template(source="""%require(name)
+<html>
+Hello World!<br><br>
+  Hello {{name|e}}<br><br>
+%for i in range(len(name)):
+  Hello {{name[:i+1]|e}}<br>
+%endfor
+</html>""")
+
+
+@get('/')
+@get('/(?P<name>\w*)')
+def index(request, name='stranger'):
+    return index_template.render(name=name)
+
+
+run_fiole()
